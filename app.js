d3c8dda109753645f7ab99ec11cbacfbbb271ab1
VirtualityUser <135662926+VirtualityUser@users.noreply.github.com>
2023-06-08 02:51:41 +0530

    resolve cross origin policy

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
+
 // Endpoint for sending emails
 app.post('/send-email', async (req, res) => {
   const { to, subject, text } = req.body;
